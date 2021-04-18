5b7e1d32c0d0be4fdac8cd7f59e134e76a82f388
khongtrunght <khongtrunght@gmail.com>
2021-04-18 14:35:34 +0700

    tf_idf

---
+++ src/compute_tf_idf.py
@@ -0,0 +1,50 @@
+import os
+import numpy as np
+
+def get_tf_idf(data_path):
+    # get pre_compute data from file
+    dirname = os.path.dirname(__file__)
+    with open(os.path.join(dirname, '../datasets/20news-bydate/words_idfs.txt'), 'r') as f:
+        words_idfs = [(line.split('<fff>')[0], float(line.split('<fff>')[1]))
+                      for line in f.read().splitlines()]
+        word_IDs = dict([(word, index) for
+                         index, (word, idf) in enumerate(words_idfs)])
+        idfs = dict(words_idfs)
+
+    with open(data_path, 'r') as f:
+        documents = [
+            (int(line.split('<fff>')[0]),
+             int(line.split('<fff>')[1]),
+             line.split('<fff>')[2])
+            for line in f.read().splitlines()
+        ]
+        data_tf_idf = []
+        for document in documents:
+            label, doc_id, text = document
+            words = [word for word in text.split() if word in idfs]
+            word_set = list(set(words))
+            max_term_freq = max([words.count(word) for word in word_set])
+            words_tfidfs = []
+            sum_squares = 0.0
+            for word in word_set:
+                term_freq = words.count(word)
+                tf_idf_value = term_freq * 1. / max_term_freq * idfs[word]
+                words_tfidfs.append((word_IDs[word], tf_idf_value))
+                sum_squares += tf_idf_value ** 2
+
+            words_tfidfs_normalized = [str(index) + ':'
+                                           + str(tf_idf_value / np.sqrt(sum_squares))
+                                           for index, tf_idf_value in words_tfidfs]
+            sparse_rep = ' '.join(words_tfidfs_normalized)
+            data_tf_idf.append((label, doc_id, sparse_rep))
+        
+        dirname = os.path.dirname(__file__)
+        path = os.path.join(dirname, '../datasets/20news-bydate/data_tf_idf.txt')
+        with open(path, 'w') as f:
+            f.write('\n'.join([str(label) + '<fff>' + str(doc_id) + '<fff>' + sparse_rep for label, doc_id, sparse_rep in data_tf_idf]))
+
+
+if __name__ == '__main__':
+    dirname = os.path.dirname(__file__)
+    path = os.path.join(dirname, '../datasets/20news-bydate/20news-test-processed.txt')
+    get_tf_idf(path)
--- test/test1.py
+++ test/test1.py
@@ -1,26 +1,50 @@
-# %% 
-import numpy as np
+#%%
+import os
+from os import listdir
+from os.path import isfile
+import re
 
+#%%
+def gather_20newsgroups_data():
+    dirname = os.path.dirname(__file__)
+    path = os.path.join(dirname, '../datasets/20news-bydate/')
+    # path = '../datasets/20news-bydate/'
+    dirs = [path + sub_dir_name + '/' 
+            for sub_dir_name in listdir(path)
+            if not isfile(path + sub_dir_name) ]
+    train_dir, test_dir = (dirs[0], dirs[1]) if 'train' in dirs[0] \
+        else (dirs[1], dirs[0])
+    # get list of the newsgorups in train_dir
+    list_newsgroups = [newsgroup
+                        for newsgroup in  listdir(train_dir)]
+    list_newsgroups.sort()
+    return list_newsgroups
+#%%
+def collect_data_from(parent_dir, newsgroup_list):
+    data = []
+    from nltk.stem.porter import PorterStemmer
+    stemmer = PorterStemmer()
+    for group_id, newsgroup in enumerate(newsgroup_list):
+        label = group_id
+        dir_path = parent_dir + '/' + newsgroup + '/'
+        files = [(filename, dir_path + filename)
+                for filename in listdir(dir_path)
+                if isfile(dir_path + filename)]
+        files.sort()
+        for filename, filepath in files:
+            with open(filepath) as f:
+                text = f.read().lower()
+                # remove stop words
+                words = [stemmer.stem(word) for word in re.split('\W+',text)]
 
-a = np.array([1,2,3,4])
-l = [1,2,3,4]
-def test(b):
-    global a
-    arr = [0,1,2,3]
-    np.random.shuffle(arr)
-    b = b[arr]
-    print(b)
-
-def test1(l):
-    l = [3,4,5,6]
-
-
-
-
-print(a)
-test(a)
-print(a)
+#%%
+filepath = '../datasets/20news-bydate/20news-bydate-train/comp.graphics/37913'
+from nltk.stem.porter import PorterStemmer
+stemmer = PorterStemmer()
+with open(filepath) as f:
+    text = f.read().lower()
+    # print(text)
+    # print(re.split('\W+', text))
+    words = [stemmer.stem(word) for word in re.split('\W+',text)]
+    print(words)
 # %%
-print(l)
-test1(l)
-print(l)
